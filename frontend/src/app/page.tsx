79e6b588b9b2389ef6c1ae1ba790249b5e2c639f
mgkkmg <>
2024-10-19 12:52:49 +0900

    refactor: frontend 경로 수정

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Monitoring from './monitoring';
+import Monitoring from './pages/monitoring';
 
 export default function Home() {
     return (
@@ -7,7 +7,7 @@ export default function Home() {
             <Head>
                 <title>Bitcoin Trading Dashboard</title>
                 <meta name="description" content="Monitor your automated Bitcoin trading" />
-                <link rel="icon" href="../favicon.ico" />
+                <link rel="icon" href="./favicon.ico" />
             </Head>
 
             <main>
